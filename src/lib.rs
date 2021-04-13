d2795fa9c77b03292753dde636166be73871538c
printfn <printfn@users.noreply.github.com>
2021-04-13 16:43:01 +1200

    Add example to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+//! # bsa
+//!
+//! Here is an example of how to use this library:
+//!
+//! ```no_run
+//! use std::error::Error;
+//!
+//! fn main() -> Result<(), Box<dyn Error>> {
+//!     let mut bsa = bsa::open("file.bsa")?;
+//!     for folder in bsa.folders() {
+//!         for file in folder.files() {
+//!             println!("File {:?} in folder {:?}", file.name(), folder.name());
+//!             let contents = file.read_to_vec(&mut bsa)?;
+//!             println!("{:?}", &contents);
+//!         }
+//!     }
+//!     Ok(())
+//! }
+//! ```
+
 mod bsa;
 mod cp1252;
 mod hash;
